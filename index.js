ee1d83afb32aa56d30673fb325fe64927cb0b1a0
Sammuel Reis <sammugr@hotmail.com>
2020-01-31 20:39:44 -0300

    route put projects

--- index.js
+++ index.js
@@ -29,4 +29,13 @@ server.post('/projects', (req, res) => {
   return res.json(projects)
 }); // creates new project
 
+server.put('/projects/:id', (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+
+  projects[id-1].title = title;
+
+  return res.json(projects);
+}); // updates project title
+
 server.listen(3000);
