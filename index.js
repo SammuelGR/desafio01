15d706c88e2597959a1b75525456b433ea6eba81
Sammuel Reis <sammugr@hotmail.com>
2020-01-31 20:34:09 -0300

    route post projects

--- index.js
+++ index.js
@@ -6,14 +6,27 @@ server.use(express.json());
 
 const projects = [
   {
-    id: "1",
-    title: "Novo projeto",
-    tasks: ["Nova tarefa"],
+    id: "0",
+    title: "Primeiro projeto",
+    tasks: ["Primeira tarefa"],
   }
 ];
 
 server.get('/projects', (req, res) => {
   return res.json(projects);
-})
+}); // lists all projects
+
+server.post('/projects', (req, res) => {
+  const { id, title } = req.body;
+  const project = {
+    id,
+    title,
+    tasks: []
+  };
+
+  projects.push(project);
+
+  return res.json(projects)
+}); // creates new project
 
 server.listen(3000);
