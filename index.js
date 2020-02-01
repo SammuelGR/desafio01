1498b24cc0d1eb7b518b75665b94ada2e58516ae
Sammuel Reis <sammugr@hotmail.com>
2020-01-31 21:12:41 -0300

    creates put route to create new task

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ server.use(express.json());
 
 const projects = [
   {
-    id: "0",
+    id: "1",
     title: "Primeiro projeto",
     tasks: ["Primeira tarefa"],
   }
@@ -47,6 +47,18 @@ server.delete('/projects/:id', (req, res) => {
   projects.splice(pIndex, 1);
   
   return res.send();
-});
+}); // deletes project
+
+server.post('/projects/:id/tasks', (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+
+  const task = title;
+  const project = projects.find(p => p.id == id);
+  
+  project.tasks.push(task);
+
+  return res.json(projects);
+}); // creates new task
 
 server.listen(3000);
